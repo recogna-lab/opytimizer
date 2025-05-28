3691893957d106b86045364fa55c77ac1b15823c
Guilherme N. Marques <guilherme.n.marques@unesp.br>
2025-05-28 19:46:28 -0300

    ref_dirs method refactored

--- opytimizer/utils/weights_vector.py
+++ opytimizer/utils/weights_vector.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def ref_dirs(n_objectives: int, n_partitions: int) -> np.ndarray:
+def ref_dirs(n_objectives: int, n_partitions: int) -> tuple[np.ndarray,int]:
     """
     Generates reference direction vectors (weight vectors) on the unit simplex.
 
@@ -10,6 +10,7 @@ def ref_dirs(n_objectives: int, n_partitions: int) -> np.ndarray:
 
     Returns:
     - ref_dirs: np.ndarray of shape (N, n_objectives), where each row is a weight vector
+    - n_subproblems: an integer that corresponds to the number of subproblems (each weight is related with a subproblem)
     """
     grid = np.mgrid[(slice(0, n_partitions+1),) * (n_objectives-1)]
     grid = grid.reshape(n_objectives-1, -1).T
@@ -25,4 +26,4 @@ def ref_dirs(n_objectives: int, n_partitions: int) -> np.ndarray:
     # Remove duplicates
     unique_weights = np.unique(weights, axis=0)
     
-    return unique_weights 
+    return unique_weights,unique_weights.shape[0]
