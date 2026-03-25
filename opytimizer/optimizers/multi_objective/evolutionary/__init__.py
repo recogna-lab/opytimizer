c2d491b3b3526bdd8ec5d43af8dedd3f303edd5d
Guilherme N. Marques <guilherme.n.marques@unesp.br>
2026-03-25 19:53:40 -0300

    optimizers(fix): fixing a possible merge problem.

--- opytimizer/optimizers/multi_objective/evolutionary/__init__.py
+++ opytimizer/optimizers/multi_objective/evolutionary/__init__.py
@@ -7,4 +7,5 @@ from opytimizer.optimizers.multi_objective.evolutionary.nsga2 import NSGA2
 from opytimizer.optimizers.multi_objective.evolutionary.spea2 import SPEA2
 from opytimizer.optimizers.multi_objective.evolutionary.moce import MOCE
 from opytimizer.optimizers.multi_objective.evolutionary.obmoce import OBMOCE
+
 from opytimizer.optimizers.multi_objective.evolutionary.nsga3 import NSGA3
