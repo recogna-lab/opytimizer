b49e7de8733ff9a9b637dcb24b5b12628d0a43a0
Guilherme N. Marques <157069835+Guilh3rm3Marqu3s@users.noreply.github.com>
2026-09-17 15:19:14 -0300

    opytimizer.core package initializator updated.

--- opytimizer/core/__init__.py
+++ opytimizer/core/__init__.py
@@ -2,12 +2,9 @@
 """
 
 from opytimizer.core.agent import Agent
-from opytimizer.core.block import InnerBlock, InputBlock, OutputBlock
-from opytimizer.core.cell import Cell
 from opytimizer.core.environment import Environment
 from opytimizer.core.function import Function
 from opytimizer.core.hyperheuristic import HyperHeuristic
-from opytimizer.core.node import Node
 from opytimizer.core.optimizer import (
     MultiObjectiveOptimizer,
     Optimizer,
